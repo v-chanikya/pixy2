ccbd8f786cf3a53b38e0db861970410b647d13c6
v chanikya <chanikya4vr1@gmail.com>
2019-09-28 22:10:34 +0530

    Initial commit at an attemtp to video stream over network
    There is an example get_raw_frame which I simply renamed
    to raw_frame_server, the idea is to create a server and
    when a client connects start reading from pixy and send it
    over network to client
    
    Signed-off-by: v chanikya <chanikya4vr1@gmail.com>

---
+++ scripts/build_raw_frame_server.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+function WHITE_TEXT {
+  printf "\033[1;37m"
+}
+function NORMAL_TEXT {
+  printf "\033[0m"
+}
+function GREEN_TEXT {
+  printf "\033[1;32m"
+}
+function RED_TEXT {
+  printf "\033[1;31m"
+}
+
+WHITE_TEXT
+echo "########################################################################################"
+echo "# Building Get Raw Frame...                                                            #"
+echo "########################################################################################"
+NORMAL_TEXT
+
+uname -a
+
+TARGET_BUILD_FOLDER=../build
+
+mkdir $TARGET_BUILD_FOLDER
+mkdir $TARGET_BUILD_FOLDER/raw_frame_server
+
+rm $TARGET_BUILD_FOLDER/raw_frame_server/raw_frame_server
+cd ../src/host/libpixyusb2_examples/raw_frame_server
+make
+mv ./raw_frame_server ../../../../build/raw_frame_server
+
+if [ -f ../../../../build/raw_frame_server/raw_frame_server ]; then
+  GREEN_TEXT
+  printf "SUCCESS "
+else
+  RED_TEXT
+  printf "FAILURE "
+fi
+echo ""
